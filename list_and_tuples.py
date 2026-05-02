c7c208bd17a1b5d56bdc685a2e22ff4a9e6b4cad
Humaira Bibi <humairabaksh38@gmail.com>
2026-05-02 21:18:09 +0500

    list and tuplels

--- list_and_tuples.py
+++ list_and_tuples.py
@@ -4,6 +4,12 @@
 #tuples are faster than list
 
 
+from shutil import copy
+
+
+import copy
+
+
 marks = [56,25,10, 90]
 print(marks)
 print(len(marks))
@@ -76,7 +82,78 @@ print(list)
 
 #tuples are faster than list
 
-tuples = (56,25,"Humaira", 90)
-print(tuples)
-print(type(tuples[0]))
-print(type(tuples[2]))
+# tuples = (56,25,"Humaira", 90)
+# print(tuples)
+# print(type(tuples[0]))
+# print(type(tuples[2]))
+# tuples [2] = "Bibi" #error dega q k is mai change nhi kr skte tu error ajaega
+
+# tup = ()
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3, 4, 5)
+# print(tup)
+# print(type(tup)) #tuple aayega kyu kyu k ek element wala tuple bna hai tu python usko tuple samjhta hai
+
+# tup = (1, 2, 3, 4, 5)
+# print(tup[1:3]) #1 se 2 tk print kr dega 3 tk nhi kyu k 3rd index tk print krna hai tu 2 tk print kr dega
+
+# #Tuple Methods:
+
+# tup = (2,1,3,1)
+# print(tup.count(1)) #1 kitni baar aya hai uska count deta hai
+# print(tup.index(3)) #3 ka index deta hai
+
+# #Practice Questions:
+
+
+# lists = []
+# listing1 = input("Enter your 1st favorite movies:")
+# lists.append(listing1)
+
+# listing2 = input("Enter your 2nd favorite movies:")
+# lists.append(listing2)
+
+# listing3 = input("Enter your 3rd favorite movies:")
+# lists.append(listing3)
+
+# print(lists)
+
+#Palindrom in lists  => start and end jahn se bhi prhein word same like racecar , madam, level, rotor, kayak, reviver, madam, refer, deed, noon
+
+list1 = [1,2,1]
+list2 = [1,2,3]
+
+# copy.list1 = list1.copy() #list1 ko copy krke copy.list1 mai store kr dega
+# copy.list1.reverse() #copy.list1 ko reverse kr dega
+# if(list1 ==copy.list1):
+#     print("Palindrome 1")
+# else:    print("Not Palindrome")
+
+
+#input val pe palindrome check krna hai
+
+# inpt = input("Enter a word:")
+# copy.inpt = inpt[::-1] #input ko reverse krke copy.inpt mai store kr dega
+# if(inpt ==copy.inpt):
+#     print("Palindrome")
+# else:    print("Not Palindrome")
+
+
+#std js ka grade A hai
+
+grade = ("C-", "A"  ,"B", "A+", "C-", "A" , "A")
+print("Grade A occurs",grade.count("A"),"times")    #A kitni baar aya hai uska count deta hai
+
+#from A -> D
+# grade_lis = ["C-", "A"  ,"B", "A+", "C-", "A" , "A" , "B-", "D"]
+# print(grade_lis.sort())
+# print(grade_lis)
+
+
+
+# From D -> A
+grade_lis = ["C-", "A"  ,"B", "A+", "C-", "A" , "A" , "B-", "D"]
+print(grade_lis.sort(reverse= True))
+print(grade_lis)
